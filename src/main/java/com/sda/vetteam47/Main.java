1b27c7a8a599d5dbedfc6ca4445384d47fa28ad2
Cristi Buzzi <buzzi_cristi@yahoo.com>
2023-03-23 22:13:51 +0200

    Test 2

--- src/main/java/com/sda/vetteam47/Main.java
+++ src/main/java/com/sda/vetteam47/Main.java
@@ -3,5 +3,8 @@ package com.sda.vetteam47;
 public class Main {
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
     }
 }
