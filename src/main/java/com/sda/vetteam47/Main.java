186654cd1a27a8ea6f6b830b491d516aa2d73908
Stefan Zabrian <faniss2005@yahoo.com>
2023-03-25 12:21:36 +0200

    Creating Tickets and Updating Main class

--- src/main/java/com/sda/vetteam47/Main.java
+++ src/main/java/com/sda/vetteam47/Main.java
@@ -2,9 +2,5 @@ package com.sda.vetteam47;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
-        System.out.println("test");
-        System.out.println("test");
-        System.out.println("test");
     }
 }
