b1ea6f7d0b306a4eb9798fc344b751f9bb86f66d
Stefan Zabrian <faniss2005@yahoo.com>
2023-03-28 16:24:54 +0300

    Testing Instance for Session in main

--- src/main/java/com/sda/vetteam47/Main.java
+++ src/main/java/com/sda/vetteam47/Main.java
@@ -1,6 +1,10 @@
 package com.sda.vetteam47;
 
+import com.sda.vetteam47.utils.SessionManager;
+
 public class Main {
     public static void main(String[] args) {
+        SessionManager.getSessionFactory();
+        SessionManager.shutDown();
     }
 }
