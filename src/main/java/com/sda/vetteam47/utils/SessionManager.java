f4f1e928a355de3fbdfab8a36ca5d344da6479ec
Stefan Zabrian <faniss2005@yahoo.com>
2023-03-28 17:30:53 +0300

    VT-2 Setup Hibernate Model

--- src/main/java/com/sda/vetteam47/utils/SessionManager.java
+++ src/main/java/com/sda/vetteam47/utils/SessionManager.java
@@ -1,5 +1,8 @@
 package com.sda.vetteam47.utils;
 
+import com.sda.vetteam47.model.Consult;
+import com.sda.vetteam47.model.Pet;
+import com.sda.vetteam47.model.Vet;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
@@ -19,5 +22,8 @@ public class SessionManager extends AbstractSessionManager{
     }
     @Override
     protected void setAnnotatedClasses(Configuration configuration) {
+        configuration.addAnnotatedClass(Vet.class);
+        configuration.addAnnotatedClass(Pet.class);
+        configuration.addAnnotatedClass(Consult.class);
     }
 }
